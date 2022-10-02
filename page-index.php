78c7a5e19a6afd30e2895e4ee8199d757c098c4b
liyanxie <xlyustc@gmail.com>
2022-10-02 09:38:05 -0700

    Add files via upload

---
+++ page-index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<meta name="generator" content="jemdoc, see http://jemdoc.jaboc.net/" />
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<link rel="stylesheet" href="jemdoc.css" type="text/css" />
+<title>Liyan Xie</title>
+<!-- MathJax -->
+<script type="text/javascript" async src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-MML-AM_CHTML">
+</script>
+<script type="text/x-mathjax-config">
+MathJax.Hub.Config({
+	  TeX: { equationNumbers: { autoNumber: "AMS" } }
+});
+</script>
+<!-- End MathJax -->
+</head>
+<body>
+<table summary="Table for page layout." id="tlayout">
+<tr valign="top">
+<td id="layout-menu">
+<div class="menu-item"><a href="index.html" class="current">Home</a></div>
+<div class="menu-item"><a href="bio.html">Biography</a></div>
+<div class="menu-item"><a href="publications.html">Publications</a></div>
+<div class="menu-item"><a href="awards.html">Awards</a></div>
+<div class="menu-item"><a href="teaching.html">Teaching</a></div>
+</td>
+<td id="layout-content">
+<div id="toptitle">
+<h1>Liyan Xie</h1>
+</div>
+<table class="imgtable"><tr><td>
+<img src="B83A1064-LiyanXie_Web.jpg" alt="" width="110cm" height="150cm" />&nbsp;</td>
+<td align="left"><p>Liyan (Leanne) Xie <br />
+Ph.D. Candidate <br />
+<a href="https://www.isye.gatech.edu" target=&ldquo;blank&rdquo;>H. Milton Stewart School of Industrial and Systems Engineering(ISyE)</a> <br />
+<a href="https://www.gatech.edu" target=&ldquo;blank&rdquo;>Georgia Institute of Technology</a> <br /></p>
+<p><a href="https://www.prism.gatech.edu/~lxie49/CV_LiyanXIE.pdf" target=&ldquo;blank&rdquo;><b>CV</b></a> <br />
+<a href="https://scholar.google.com/citations?user=KtLwkBYAAAAJ&amp;hl=en" target=&ldquo;blank&rdquo;><b>Google Scholar</b></a></p>
+</td></tr></table>
+<h2>Contact</h2>
+<ul>
+<li><p>E-Mail: <i>lxie49</i> [@] <i>gatech</i> [DOT] <i>edu</i> </p>
+</li>
+<li><p>Address: Office 415, Main Building, 755 Ferst Drive NW, Atlanta, GA 30332</p>
+</li>
+</ul>
+<h2>Research</h2>
+<p>Advisor: Professor <a href="https://www2.isye.gatech.edu/~yxie77/" target=&ldquo;blank&rdquo;>Yao Xie</a></p>
+<p>My research interest lies in statistics and optimization, with a primary focus on sequential change-point detection and their applications in sensor networks and industrial quality control. </p>
+<p>I am very fortunate to collaborate with Professors <a href="http://www.ssp.ece.upatras.gr/moustakides/" target=&ldquo;blank&rdquo;>George V. Moustakides</a>, Professor <a href="https://faculty.mccombs.utexas.edu/rui.gao/" target=&ldquo;blank&rdquo;>Rui Gao</a>, Professor <a href="https://noise.earth.utah.edu" target=&ldquo;blank&rdquo;>Fan-Chi Lin</a>, and Professor <a href="http://sensorweb.engr.uga.edu/index.php/song/" target=&ldquo;blank&rdquo;>WenZhan Song</a>.</p>
+<h2>News</h2>
+<p>November 2019, received the <a href="http://www.arc.gatech.edu/hg/item/630228" target=&ldquo;blank&rdquo;>ARC-TRIAD fellowship</a> for the spring semester 2020. <br /></p>
+<p>October 2019, received the third prize for poster competition in <a href="http://pwp.gatech.edu/gsd2019/" target=&ldquo;blank&rdquo;>Georgia Statistics Day 2019</a>. <br /></p>
+<p>September 2019, enter into the finalist of 2019 <a href="https://connect.informs.org/qsr/home" target=&ldquo;blank&rdquo;>INFORMS QSR</a> best student paper competition for the paper entitled &lsquo;&lsquo;Optimal Sequential Subspace Change-point Detection&rsquo;&rsquo;. <br /></p>
+<p>August 2019, selected as a runner-up for the INFORMS Computing Society Student Paper Prize for paper &lsquo;&lsquo;Robust Hypothesis Testing with Wasserstein Uncertainty Sets&rsquo;&rsquo;. <br /></p>
+<p>August 2019, attended the <a href="http://pwp.gatech.edu/fds-summer-school/" target=&ldquo;blank&rdquo;>Foundation of Data Science Summer School</a> sponsored by the NSF TRIPODS Institutes at the Georgia Institute of Technology. <br /></p>
+<p>May 2019, received the Travel Grant from the <a href="https://2019.ieee-isit.org" target=&ldquo;blank&rdquo;>ISIT 2019</a> conference, and will present the paper <a href="https://arxiv.org/abs/1901.08196" target=&ldquo;blank&rdquo;>&ldquo;Asynchronous multi-sensor change-point detection for seismic tremors&rdquo;</a>, joint work with George V. Moutakides and Yao Xie. <br /></p>
+<p>May 2019, presented a poster &ldquo;Robust Hypothesis Testing Using Wasserstein Uncertainty Sets&rdquo; in <a href="https://pwp.gatech.edu/wufest/" target=&ldquo;blank&rdquo;>WuFest</a> and won the best poster award. <br /></p>
+<p>February 2019, attended the <a href="https://csl.illinois.edu/events/featured/14th-csl-student-conference" target=&ldquo;blank&rdquo;>14th CSL Student Conference</a> held in UIUC. </p>
+<div id="footer">
+<div id="footer-text">
+Page generated 2020-06-26 10:09:32 EDT, by <a href="https://github.com/wsshin/jemdoc_mathjax" target="blank">jemdoc+MathJax</a>.
+</div>
+</div>
+</td>
+</tr>
+</table>
+</body>
+</html>
